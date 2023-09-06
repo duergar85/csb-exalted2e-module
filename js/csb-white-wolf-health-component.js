0b6a786c57cae7ef856dda62b50ca80b170e4eee
duergar85 <daniel.ing.guarnieri@gmail.com>
2023-09-06 19:11:44 +0200

    Loading Component issue fixed - v1.0.1

--- js/csb-white-wolf-health-component.js
+++ js/csb-white-wolf-health-component.js
@@ -1,8 +1,7 @@
 import WWHealthCounter from "../js/WWHealthCounter.js";
 
-console.log('IT WORKS!');
-
-Hooks.once('customSystemBuilderReady', () => {
-	console.log("Hello there!");
+Hooks.once('init', () => {
+	console.log("CBS WW Health Counter | Module is loading...");
     componentFactory.addComponentType('wWHealthCounter', WWHealthCounter);
+	console.log("CBS WW Health Counter | Module is loaded!");
 });
